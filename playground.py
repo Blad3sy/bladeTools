b0361e69dde0a92a368db012e1f1ebe9f3fac72e
Bladesy <oscarprosser@gmail.com>
2023-09-12 20:32:07 +0100

    Added merge sort
    Added a merge sort algorithm to the sorting algorithms

--- playground.py
+++ playground.py
@@ -1,22 +1,32 @@
 import pyTools
 from random import randint
 
-array = [9, 10, 2, 3, 8, 4, 7, 6, 5, 10, 1, 4]
+array = []
 
-print(pyTools.bubble_Sort(array))
+for i in range(0, 10000):
+    array.append(randint(0, 10000))
 
-array = [9, 10, 2, 3, 8, 4, 7, 6, 5, 10, 1, 4]
-print()
-print(pyTools.insertion_Sort(array))
+print("NUM GEN 1")
 
-stack = pyTools.Stack()
-for i in range(0, 5):
-    stack.push(randint(0, 100))
+pyTools.bubble_Sort(array)
+print("BUBBLE DONE")
 
-print(stack.list)
+array = []
 
-for i in range(0, 5):
-    print(stack.peek())
-    stack.pop()
+for i in range(0, 10000):
+    array.append(randint(0, 10000))
 
-print(stack.empty())
+print("NUM GEN 2")
+
+pyTools.insertion_Sort(array)
+print("INSERT DONE")
+
+array = []
+
+for i in range(0, 10000):
+    array.append(randint(0, 10000))
+
+print("NUM GEN 3")
+
+print(pyTools.merge_Sort(array))
+print("merge DONE")
--- pyTools.py
+++ pyTools.py
@@ -60,7 +60,47 @@ def insertion_Sort(array):
     return array
 
 def merge_Sort(array):
-    pass
+    mainArray = []
+    auxArray = []
+    subArray = []
+
+    for item in array:
+        mainArray.append([item])
+    
+    while len(mainArray) != 1:
+        for i in range(0, len(mainArray) - 1, 2):
+            subcounter1 = 0
+            subcounter2 = 0
+
+            for t in range(0, len(mainArray[0]) * 2):
+
+                if subcounter1 >= len(mainArray[i]):
+                    for z in range(subcounter2, len(mainArray[i+1])):
+                        subArray.append(mainArray[i+1][z])
+                    break
+
+                elif subcounter2 >= len(mainArray[i+1]):
+                    for z in range(subcounter1, len(mainArray[i])):
+                        subArray.append(mainArray[i][z])
+                    break
+              
+                if mainArray[i][subcounter1] < mainArray[i+1][subcounter2]:
+                    subArray.append(mainArray[i][subcounter1])
+                    subcounter1 += 1
+                else:
+                    subArray.append(mainArray[i+1][subcounter2])
+                    subcounter2 += 1
+            
+            auxArray.append(subArray)
+            subArray = []
+                
+        if len(mainArray) % 2:
+            auxArray.append(mainArray[len(mainArray) - 1])
+    
+        mainArray = auxArray
+        auxArray = []
+    
+    return mainArray[0]
 
 # Data structures
 
