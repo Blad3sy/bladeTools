ffda08d50997d7dc8db51c54c10b402aed6e8ef5
Blad3sy <oscarprosser@gmail.com>
2023-09-18 18:45:47 +0100

    Added many graphs
    - Fixed Issue #3
    - Changed Unweighted_Graph to Directed_Graph and polished it a little
    - Added Undirected_Graph, Weighted_Graph and Undirected_Weighted_Graph

--- pyTools/data_structures.py
+++ pyTools/data_structures.py
@@ -135,7 +135,17 @@ class Linked_List():
         except KeyError: print("No entry exists for this pointer!")
     
     def addNode(self, value, pointer = None):
-        self.dict[self.size()] = [value, pointer]
+        if self.dict[pointer] or pointer == None:
+            self.dict[self.size()] = [value, pointer]
+        else: print("Your specified pointer has no destination!")
+    
+    def changeValue(self, newValue, index):
+        try: self.dict[index][0] = newValue
+        except KeyError: print("No entry exists for this index!")
+    
+    def changePointer(self, newPointer, index):
+        try: self.dict[index][1] = newPointer
+        except KeyError: print("No entry exists for this index!")
     
     def deleteNode(self, index):
         if self.dict:
@@ -150,10 +160,9 @@ class Linked_List():
     def clear(self):
         self.dict = {}
 
-class Unweighted_Graph():
+class Directed_Graph():
     def __init__(self):
         self.dict = {}
-        self.currentNodeKey = None
     
     def empty(self):
         if self.dict: return False
@@ -162,8 +171,10 @@ class Unweighted_Graph():
     def size(self):
         return len(self.dict)
     
-    def addNode(self, key, connections, data):
-        self.dict[key] = {"connections" : connections, "data" : data}
+    def addNode(self, key, data, edges = None):
+        if type(edges) == list or edges == None:
+            self.dict[key] = {"edges" : edges, "data" : data}
+        else: print("The edges were not in the form of a list!")
     
     def deleteNode(self, key):
         if self.dict: 
@@ -171,9 +182,9 @@ class Unweighted_Graph():
             except KeyError: print("No entry exists for this key!")
         else: print("The graph is empty!")
     
-    def viewConnections(self, key):
+    def viewEdges(self, key):
         if self.dict: 
-            try: return self.dict[key]["connections"]
+            try: return self.dict[key]["edges"]
             except KeyError: print("No entry exists for this key!")
         else: print("The graph is empty!")
 
@@ -183,5 +194,119 @@ class Unweighted_Graph():
             except KeyError: print("No entry exists for this key!")
         else: print("The graph is empty!")
     
+    def changeEdges(self, key, edges):
+        if type(edges) == list:
+            self.dict[key]["edges"] = edges
+        else: print("The edges were not in the form of a list!")
+    
+    def changeData(self, key, data):
+        self.dict[key]["data"] = data
+    
     def clear(self):
         self.dict = {}
+
+class Undirected_Graph(Directed_Graph):
+    def __init__(self, ignoreInvalidEdges = False):
+        super().__init__()
+        self.ignoreInvalidEdges = ignoreInvalidEdges
+
+    def addNode(self, key, data, edges = []):
+        if type(edges) == list:
+            self.dict[key] = {"edges" : edges, "data" : data}
+        else: print("The edges were not in the form of a list!")
+
+        keys = self.dict.keys()
+
+        for i in range(0, len(edges)):
+            if edges[i] in keys: self.dict[key]["edges"].append(edges[i])
+            else: 
+                if not self.ignoreInvalidEdges: print("A node you referenced in your edges does not exist!")
+
+    def deleteNode(self, key):
+        if self.dict: 
+            try:
+                keys = self.dict.keys() 
+                for akey in keys:
+                    for i in len(self.dict[akey]["edges"]):
+                        if self.dict[akey]["edges"][i] in self.dict[key]["edges"]:
+                            del self.dict[akey]["edges"][i]            
+                del self.dict[key]                 
+            except KeyError: print("A key was used for an invalid node!")
+        else: print("The graph is empty!")
+    
+    def updateEdges(self):
+
+        # when adding graph traversal PLEASE FOR THE LOVE OF GOD check this function
+
+        keys = self.dict.keys()
+
+        for key in keys:
+            for item in self.dict[key]["edges"]:
+                try: self.dict[item]["edges"].append(key)
+                except KeyError: 
+                    if not self.ignoreInvalidEdges: print(f"A node you referenced in the edges of value {key} does not exist!")
+    
+    def changeEdges(self, key, edges):
+        if type(edges) == list:
+            self.dict[key]["edges"] = edges
+        else: print("The edges were not in the form of a list!")
+        self.updateEdges()
+
+class Weighted_Graph(Directed_Graph):
+    def __init__():
+        super().__init__()
+    
+    # The change here is rather miniscule - I have simply changed the intended data type of the edges from a LIST to a DICTIONARY
+    # I have done this with the intended purpose of edge weight being input in the format of {"Edge": Edge weight, etc...}
+    # Future code designed traverse Graphs will include algorithms utilizing the weight here, my current focus is on creating the data structure
+
+    def addNode(self, key, data, edges = None):
+        if type(edges) == dict or edges == None:
+            self.dict[key] = {"edges" : edges, "data" : data}
+        else: print("The edges were not in the form of a dictionary!")
+    
+    def changeEdges(self, key, edges):
+        if type(edges) == dict:
+            self.dict[key]["edges"] = edges
+        else: print("The edges were not in the form of a dictionary!")
+    
+class Weighted_Undirected_Graph(Undirected_Graph):
+    def __init__():
+        super().__init__()
+    
+    # My notes on Weighted_Graph (Lines 249-251) also apply here. This code is different to Unidirected_Graph in the same way that Weighted_Graph is different to Directed_Graph
+
+    def addNode(self, key, data, edges = []):
+        if type(edges) == dict:
+            self.dict[key] = {"edges" : edges, "data" : data}
+        else: print("The edges were not in the form of a dictionary!")
+
+        keys = self.dict.keys()
+        edgeKeys = edges.keys()
+
+        for eKey in edgeKeys:
+            if edges[eKey] in keys: self.dict[key]["edges"][eKey] = edges[eKey]
+            else: 
+                if not self.ignoreInvalidEdges: print("A node you referenced in your edges does not exist!")
+    
+    def updateEdges(self):
+
+        # When I get around the adding the code to traverse the graph I for sure am gonna check this shit
+        # because i'm accounting for functionality I haven't added yet and it's probably broken, ah well
+
+        # i'll probably reprogram everything in this function because this entire function sucks so bad
+
+        keys = self.dict.keys()
+
+        for key in keys:
+            edgeKeys = self.dict[key]["edges"].keys()
+            for eKey in edgeKeys:
+                if eKey in keys: self.dict[eKey]["edges"][key] = self.dict[eKey]["edges"]["edgeWeight"]
+                else: 
+                    if not self.ignoreInvalidEdges: print("A node you referenced in your edges does not exist!")
+
+    def changeEdges(self, key, edges):
+        if type(edges) == dict:
+            self.dict[key]["edges"] = edges
+        else: print("The edges were not in the form of a dictionary!")
+        self.updateEdges()
--- pyTools/playground.py
+++ pyTools/playground.py
@@ -1,27 +1,13 @@
 import data_structures
 from random import randint
 
-linkedList = data_structures.Linked_List()
+undir = data_structures.Undirected_Graph()
+undir2 = data_structures.Undirected_Graph(True)
 
-linkedList.addNode(0, 1)
-linkedList.addNode(1, 2)
-linkedList.addNode(2, 3)
-linkedList.addNode(3, 4)
-linkedList.addNode(4, 5)
-linkedList.addNode(5)
+undir.addNode("A", "DATA")
+undir.addNode("B", "DATA", ["A", "C"])
 
-print(linkedList.dict)
+undir2.addNode("A", "DATA")
+undir2.addNode("B", "DATA", ["A", "C"])
 
-for i in range(0, linkedList.size()):
-    print(linkedList.nextNode())
-
-linkedList.deleteNode(2)
-linkedList.resetPointer()
-
-print(linkedList.dict)
-
-for i in range(0, linkedList.size()):
-    print(linkedList.nextNode())
-
-test = data_structures.Static_Queue()
-test.setSize("A")
+print("DONE")
