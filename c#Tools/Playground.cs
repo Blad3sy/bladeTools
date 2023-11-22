ea6b6a21870e5dfc60ee1bdb74af78f2f4b48480
Bladesy <71397567+Blad3sy@users.noreply.github.com>
2023-11-22 20:49:01 +0000

    Merge sort added
    -sorting_algorithms.cs now has Merge Sort (although its awfully contrived - the algorithm SUCKS)

--- c#Tools/Playground.cs
+++ c#Tools/Playground.cs
@@ -1,19 +1,18 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
-using System.Threading;
 using sorting_algorithms;
 
 namespace Playground {
     class Playground {
         static void Main(string[] args) {
-            int[] arr = new int[60];
+            int[] arr = new int[59];
 
             Random rnd = new Random();
-            for (int i = 0; i < 60; i++) {
+            for (int i = 0; i < 59; i++) {
                 arr[i] = rnd.Next(0, 60);
             }
 
-            arr = Sorting_Algorithms.InsertionSort(arr);
+            arr = Sorting_Algorithms.MergeSort(arr);
 
             foreach (int item in arr) {
                 Console.Write($"{item}, ");
--- c#Tools/sorting_algorithms.cs
+++ c#Tools/sorting_algorithms.cs
@@ -55,5 +55,76 @@ namespace sorting_algorithms {
 
             return array;
         }
+
+        public static int[] MergeSort(int[] array) {
+            int[][] completeArray = new int[array.Length][];
+
+            for (int i = 0; i < array.Length; i++) {
+              completeArray[i] = new int[] {array[i]};  
+            }
+
+            while (completeArray.Length > 1) {
+                for (int i = 0; i < completeArray.Length; i += 2) {
+                    if (i+1 < completeArray.Length) {
+                        completeArray[i] = Merge(completeArray[i], completeArray[i+1]);
+                        completeArray[i+1] = Array.Empty<int>();
+                    }
+                }
+
+                completeArray = ArrayClean(completeArray);
+            }
+
+            return completeArray[0];
+        }
+
+        private static int[] Merge(int[] array1, int[] array2) {
+            int[] completeArray = new int[array1.Length + array2.Length];
+            int array1Pointer = 0;
+            int array2Pointer = 0;
+
+            int iterator = 0;
+
+            while (array1Pointer < array1.Length && array2Pointer < array2.Length) {
+                if (array1[array1Pointer] < array2[array2Pointer]) {
+                    completeArray[iterator] = array1[array1Pointer];
+                    array1Pointer++;
+                }
+                else {
+                    completeArray[iterator] = array2[array2Pointer];
+                    array2Pointer++;
+                }
+
+                iterator++;
+            }
+
+            if (array1Pointer >= array1.Length) {
+                for (int i = iterator; i < completeArray.Length; i++) {
+                    completeArray[i] = array2[array2Pointer];
+                    array2Pointer++;
+                }
+            }
+            else {
+                for (int i = iterator; i < completeArray.Length; i++) {
+                    completeArray[i] = array1[array1Pointer];
+                    array1Pointer++;
+                }
+            }
+
+            return completeArray;
+        }
+
+        private static int[][] ArrayClean(int[][] array) {
+            int adder = array.Length % 2;
+            int[][] newray = new int[array.Length / 2 + adder][];
+
+            for (int i = 0; i < newray.Length; i++) {
+                newray[i] = array[i*2];
+            }
+
+            if (adder == 1) {
+                newray[^1] = array[^1];
+            }
+            return newray;
+        }
     }
 }
