8317d590b1ae0bcf79d76d6c6ad617b74057b342
Bladesy <71397567+Blad3sy@users.noreply.github.com>
2023-11-27 16:58:44 +0000

    Updated c# StaticQueue
    - c# Static Queue now has method overloads for enqueue / dequeue whole arrays

--- c#Tools/Playground.cs
+++ c#Tools/Playground.cs
@@ -5,14 +5,22 @@ namespace Playground {
     class Playground {
         static void Main(string[] args) {
             int[]? starter = new int[] {1, 2, 3, 4, 5};
-            // int[] pushingArray = new int[] {6, 7, 8, 9};
+            int[] pushingArray = new int[] {8, 9, 10};
+            int[] pushingArray2 = new int[] {8, 9};
 
-            StaticQueue testQueue = new(6);
+            StaticQueue testQueue = new(6, starter);
 
+            testQueue.Dequeue(10);
             testQueue.Display();
 
-            testQueue.Enqueue(6);
-            testQueue.Enqueue(7);
+            testQueue.Dequeue(2);
+            testQueue.Dequeue(3);
+            testQueue.Display();
+
+            testQueue.Enqueue(pushingArray);
+            testQueue.Display();
+
+            testQueue.Enqueue(pushingArray2);
             testQueue.Display();
 
             Console.WriteLine(testQueue.Dequeue());
--- c#Tools/data_structures/queues.cs
+++ c#Tools/data_structures/queues.cs
@@ -3,8 +3,8 @@ using data_structure_templates;
 namespace queues {
     class StaticQueue : ArrayDataStructure {
 
-        public int HeadPointer { get; set; }
-        public int TailPointer { get; set; }
+        private int HeadPointer { get; set; }
+        private int TailPointer { get; set; }
         public StaticQueue(int length, int[]? starter = null) : base(length, starter) {
             HeadPointer = 0;
             TailPointer = (starter == null) ? 0 : starter.Length;
@@ -20,6 +20,19 @@ namespace queues {
             else { Console.WriteLine("The queue is full!"); }
         }
 
+        public void Enqueue(int[] toEnqueue) {
+            // Method overload purpose : enqueue a whole array of values to the queue
+
+            if (MainArray.Count(item => item.HasValue) + toEnqueue.Length <= Length) {
+                foreach (var item in toEnqueue) {
+                    MainArray[TailPointer] = item;
+                    TailPointer++;
+                    TailPointer = (TailPointer >= Length) ? 0 : TailPointer; 
+                }
+            }
+            else { Console.WriteLine("You tried to add more elements than possible!"); }
+        }
+
         public int? Dequeue() {
             int? returner = null;
             HeadPointer = (HeadPointer >= Length) ? 0 : HeadPointer;
@@ -30,6 +43,23 @@ namespace queues {
                 HeadPointer++;
             }
             else { Console.WriteLine("The queue is empty!"); }
+            return returner;
+        }
+
+        public int?[]? Dequeue(int count) {
+            // Method overload purpose : dequeue {count} number of items from the stack
+            int?[]? returner = null;
+
+            if (MainArray.Count(item => item.HasValue) - count >= 0) {
+                returner = new int?[count];
+                for (int i = 0; i < count; i++) {
+                    returner[i] = MainArray[HeadPointer];
+                    MainArray[HeadPointer] = null;
+                    HeadPointer++;
+                    HeadPointer = (HeadPointer >= Length) ? 0 : HeadPointer;
+                }
+            }
+            else { Console.WriteLine("You tried to dequeue more elements than there are!"); }
             return returner;
         }
     }
